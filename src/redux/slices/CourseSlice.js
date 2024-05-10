5621a7dc1d0e260db4b0c417ea34c679e47e5424
Bishal Jana <bishaljana2026@gmail.com>
2024-05-10 18:46:25 +0530

    create course

--- src/redux/slices/CourseSlice.js
+++ src/redux/slices/CourseSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 };
 export const getAllCourses = createAsyncThunk("/course/get", async () => {
   try {
-    const res = axiosIntances.get("courses");
+    const res = axiosIntances.get("/courses");
     toast.promise(res, {
       loading: "loading course data... ",
       success: "course loadad successfully",
@@ -18,6 +18,31 @@ export const getAllCourses = createAsyncThunk("/course/get", async () => {
     toast.error(error?.response?.data?.message);
   }
 });
+
+export const createNewCourse = createAsyncThunk(
+  "/course/create",
+  async (data) => {
+    try {
+      let formData = new FormData();
+      formData.append("title", data?.title);
+      formData.append("description", data?.description);
+      formData.append("category", data?.category);
+      formData.append("createdBy", data?.createdBy);
+      formData.append("thumbnail", data?.thumbnail);
+
+      const response = axiosIntances.post("/courses", formData);
+      toast.promise(response, {
+        loading: "Creating new course",
+        success: "Course created successfully",
+        error: "Failed to create course",
+      });
+
+      return (await response).data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
 
 const courseSlice = createSlice({
   name: "courses",
